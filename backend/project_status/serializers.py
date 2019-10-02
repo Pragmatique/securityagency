677519ead415c158635d6d7e477d0da6ddcdeb38
Pragmatique <Silchevihor@ukr.net>
2019-10-02 21:56:35 +0300

    added model and api for property type in the Dictionary project "project_status"

--- backend/project_status/serializers.py
+++ backend/project_status/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import ProjectStatus
+from .models import ProjectStatus, PropertyType
 
 class ProjectStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectStatus
         fields = ('id','status')
+
+class PropertyTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PropertyType
+        fields = ('id','property_type')
--- backend/project_status/urls.py
+++ backend/project_status/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import ProjectStatusViewSet
+from .views import ProjectStatusViewSet, PropertyTypeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'project-status', ProjectStatusViewSet)
+router.register(r'property-type', PropertyTypeViewSet)
 
 
 urlpatterns = [
