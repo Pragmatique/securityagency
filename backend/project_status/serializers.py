5c65f1bb0e2ffc45d31219238335386359e70d6b
Pragmatique <Silchevihor@ukr.net>
2019-10-02 23:52:14 +0300

    added model and api for service type in the Dictionary project "project_status"

--- backend/project_status/serializers.py
+++ backend/project_status/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ProjectStatus, PropertyType
+from .models import ProjectStatus, PropertyType, ServiceType
 
 class ProjectStatusSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,8 @@ class PropertyTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PropertyType
         fields = ('id','property_type')
+
+class ServiceTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceType
+        fields = ('id','service_type')
--- backend/project_status/urls.py
+++ backend/project_status/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import ProjectStatusViewSet, PropertyTypeViewSet
+from .views import ProjectStatusViewSet, PropertyTypeViewSet, ServiceTypeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'project-status', ProjectStatusViewSet)
 router.register(r'property-type', PropertyTypeViewSet)
+router.register(r'service-type', ServiceTypeViewSet)
 
 
 urlpatterns = [
